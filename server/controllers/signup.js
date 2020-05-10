61ee0c9746df229a9377d9c8038a8e40c4cb08a1
Oleh Andreiev <oandreiev@lohika.com>
2020-05-10 16:12:46 +0300

    fix auth

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,6 +1,9 @@
 const Router = require('express').Router;
 const User = require('../models/user');
+const EmailSender = require('../services/email-sender');
 const router = Router();
+
+const sender = new EmailSender();
 
 router.post('/', async (req, res) => {
   const user = new User(req.body);
@@ -12,6 +15,14 @@ router.post('/', async (req, res) => {
     }
     throw err;
   }
+
+  const message = `
+  Hello ${user.first_name},
+  please verify your email address clicking by the following
+  <a href='${req.protocol}://${req.headers.host}/signup/verify?token=${user.verify_email_token}'>link</a>
+  (Link will be valid for 6 hours)
+  `;
+  sender.sendEmail({ content: message, subject: 'Verify Email', to: user.email });
   delete user.password;
   res.send(user)
 });
@@ -20,6 +31,15 @@ router.get('/check-user', async (req, res) => {
   const { email } = req.query;
   const user = await User.findOne({ email });
   res.send({ isUserExist: !!user })
+});
+
+router.get('/verify', async (req, res) => {
+  const token = req.query.token;
+  await User.findOneAndUpdate({ verify_email_token: token }, {
+    is_verified: true,
+    verify_email_token: null
+  });
+  res.redirect('/');
 });
 
 
