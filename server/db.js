7c7f73d7fcdb13ec37b29ba5bcfaca0919c4be12
Oleh Andreiev <oandreiev@lohika.com>
2020-05-10 14:24:18 +0300

    removed extra middleware

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 // docker run --name social-network-mongo -d -p 27017:27017  mongo
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:27017/social-network', {useNewUrlParser: true, useUnifiedTopology: true});
+const DB_URL = process.env.MONGO_DB_CONNECTION_URL || 'mongodb://localhost:27017/social-network';
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
